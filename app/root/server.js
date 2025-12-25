4998a827675fa11bae02628a05ec9994e8d8515e
manuelmikozuluetadelola24-dev <manuelmikozulueta.delola24@bicol-u.edu.ph>
2025-12-26 02:50:25 +0800

    just bingoplus my location sir!

--- app/root/server.js
+++ app/root/server.js
@@ -14,11 +14,14 @@ app.use('/api', middleware, apiRoute);
 app.use('/auth', authRoute);
 app.use('/migrate', migrateRoute);
 
-app.get('/', (res, req) => {
-	res.send('ok');
+app.get('/', (err, res, req, next) => {
+	res.status(200).json({
+		"statusCode": 200,
+		"message": "okay"
+	});
 })
 
-app.use((req, res, next) => {
+app.use((err, req, res, next) => {
 	res.status(404).json({
 		"statusCode": 404,
 		"message": "endpoint not found"
