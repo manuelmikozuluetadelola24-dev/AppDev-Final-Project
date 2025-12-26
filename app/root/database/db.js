41d0d93b61d47995d0830cb2e486bfe9f5803f5f
manuelmikozuluetadelola24-dev <manuelmikozulueta.delola24@bicol-u.edu.ph>
2025-12-26 23:11:59 +0800

    u

--- app/root/database/db.js
+++ app/root/database/db.js
@@ -1,16 +1,12 @@
 const { Pool } = require('pg');
 
 class Database {
-  constructor() {
-    this.pool = new Pool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
-      ssl: { rejectUnauthorized: false } // REQUIRED on Render
-    });
-  }
+	constructor() {
+		this.pool = new Pool({
+			connectionString: process.env.DB_URL,
+			ssl: { rejectUnauthorized: false }
+		});
+	}
 
   async query(text, params) {
     const client = await this.pool.connect();
