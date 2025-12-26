0fcb995733a7677f67f751af468cd633474c4120
BU-Ace <jericacelucanas.baria24@bicol-u.edu.ph>
2025-12-26 22:22:36 +0800

    fixed db

--- app/root/database/db.js
+++ app/root/database/db.js
@@ -1,27 +1,26 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 
 class Database {
-	constructor() {
-		this.pool = new Pool({
-		host: process.env.DB_HOST,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
-		port: process.env.DB_PORT
-		});
-	}
+  constructor() {
+    this.pool = new Pool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: { rejectUnauthorized: false } // REQUIRED on Render
+    });
+  }
 
-	async query(text, params) {
-	this.client = await this.pool.connect();
-	const res = await this.client.query(text, params);
-	await this.close();
-	return res;
-	}
-  
-	async close() {
-	await this.client.end();
-	console.log('Closed database connection');
-	}
+  async query(text, params) {
+    const client = await this.pool.connect();
+    try {
+      const res = await client.query(text, params);
+      return res;
+    } finally {
+      client.release(); 
+    }
+  }
 }
-  
+
 module.exports = new Database();
